503f62c1e84b51f71af472a9d75c5bc2453d2064
Velioto <velinnikolov2007@gmail.com>
2025-04-26 20:24:36 +0300

    added a user database and connected it to recipes

--- RecipeList/Models/Recipes.cs
+++ RecipeList/Models/Recipes.cs
@@ -1,14 +1,17 @@
-﻿namespace RecipeList.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeList.Models
 {
     public class Recipes
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int UserID { get; set; }
         public int Calories { get; set; }
         public int Fats { get; set; }
         public int Carbs { get; set; }
         public int Proteins { get; set; }
         public string Description { get; set; }
+        public string UserID { get; set; }
+        public ApplicationUser User { get; set; }
     }
 }
